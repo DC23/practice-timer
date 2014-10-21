9c98356af35d531d04c52d2c299808e574dea680
Daniel Collins <jugglindan@gmail.com>
2014-10-21 21:53:27 +0800

    changed preset 1 to 1:05

--- src/countdownoptions.cpp
+++ src/countdownoptions.cpp
@@ -1,7 +1,7 @@
 #include "countdownoptions.h"
 
 CountdownOptions::CountdownOptions() :
-    presetOne(0,1,0),
+    presetOne(0,1,05),
     presetTwo(0,5,0),
     presetThree(0,10,0),
     presetFour(0,25,0),
